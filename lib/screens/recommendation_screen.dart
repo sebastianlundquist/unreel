56651fd965e94e3873007b60218400c0e20a472c
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-08-21 22:38:05 +0200

    Implement fade-in background image

--- lib/screens/recommendation_screen.dart
+++ lib/screens/recommendation_screen.dart
@@ -23,78 +23,68 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
   String title;
   String description;
 
+  dynamic img = AssetImage('images/transparent.png');
+
   void updateUI(dynamic movieData, int index) {
     setState(() {
       movieObject = Movie.fromJson(movieData['results'][index]);
       print(movieObject.title);
       movieIndex++;
+      img = NetworkImage('$imageURL$imageSize${movieObject.posterPath}');
     });
-  }
-
-  DecorationImage backgroundImage(String imageURL) {
-    if (imageURL == null) {
-      return DecorationImage(
-        image: NetworkImage(
-            'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg'),
-        fit: BoxFit.cover,
-      );
-    } else {
-      return DecorationImage(
-        image: NetworkImage(imageURL),
-      );
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            image: backgroundImage(movieObject != null
-                ? '$imageURL$imageSize${movieObject.backdropPath}'
-                : 'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg'),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    flex: 9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text(movieObject != null ? movieObject.title : ''),
-                        Text(movieObject != null ? movieObject.overview : ''),
-                      ],
+        child: Stack(
+          children: <Widget>[
+            FadeInImage(
+              placeholder: AssetImage('images/transparent.png'),
+              image: img,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 9,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(movieObject != null ? movieObject.title : ''),
+                          Text(movieObject != null ? movieObject.overview : ''),
+                        ],
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: RaisedButton(
-                      child: Text('Get Movies'),
-                      onPressed: () async {
-                        if (movieIndex == 20) {
-                          movieIndex = 0;
-                          page++;
-                        }
-                        if (movieIndex == 0) {
-                          movieData = await Movies().getMovies(
-                              minReleaseDate,
-                              maxReleaseDate,
-                              minVoteCount,
-                              minVoteAverage,
-                              page);
-                        }
-                        updateUI(movieData, movieIndex);
-                      },
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Get Movies'),
+                        onPressed: () async {
+                          if (movieIndex == 20) {
+                            movieIndex = 0;
+                            page++;
+                          }
+                          if (movieIndex == 0) {
+                            movieData = await Movies().getMovies(
+                                minReleaseDate,
+                                maxReleaseDate,
+                                minVoteCount,
+                                minVoteAverage,
+                                page);
+                          }
+                          updateUI(movieData, movieIndex);
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
