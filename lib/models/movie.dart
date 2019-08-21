b3c1d527382d0d72c364b6727a200eb728f2addf
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-08-21 21:47:51 +0200

    Add background images

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class Movie {
   final int voteCount;
   final int id;
--- lib/screens/recommendation_screen.dart
+++ lib/screens/recommendation_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/services/movies.dart';
 import 'package:movie_app/models/movie.dart';
 
+const imageURL = 'https://image.tmdb.org/t/p';
+const imageSize = '/w500';
+
 class RecommendationScreen extends StatefulWidget {
   @override
   _RecommendationScreenState createState() => _RecommendationScreenState();
@@ -28,43 +31,68 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
     });
   }
 
+  DecorationImage backgroundImage(String imageURL) {
+    if (imageURL == null) {
+      return DecorationImage(
+        image: NetworkImage(
+            'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg'),
+        fit: BoxFit.cover,
+      );
+    } else {
+      return DecorationImage(
+        image: NetworkImage(imageURL),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 9,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(movieObject != null ? movieObject.title : ''),
-                      Text(movieObject != null ? movieObject.overview : ''),
-                    ],
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: backgroundImage(movieObject != null
+                ? '$imageURL$imageSize${movieObject.backdropPath}'
+                : 'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg'),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 9,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(movieObject != null ? movieObject.title : ''),
+                        Text(movieObject != null ? movieObject.overview : ''),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: RaisedButton(
-                    child: Text('Get Movies'),
-                    onPressed: () async {
-                      if (movieIndex == 20) {
-                        movieIndex = 0;
-                        page++;
-                      }
-                      if (movieIndex == 0) {
-                        movieData = await Movies().getMovies(minReleaseDate,
-                            maxReleaseDate, minVoteCount, minVoteAverage, page);
-                      }
-                      updateUI(movieData, movieIndex);
-                    },
+                  Expanded(
+                    child: RaisedButton(
+                      child: Text('Get Movies'),
+                      onPressed: () async {
+                        if (movieIndex == 20) {
+                          movieIndex = 0;
+                          page++;
+                        }
+                        if (movieIndex == 0) {
+                          movieData = await Movies().getMovies(
+                              minReleaseDate,
+                              maxReleaseDate,
+                              minVoteCount,
+                              minVoteAverage,
+                              page);
+                        }
+                        updateUI(movieData, movieIndex);
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
