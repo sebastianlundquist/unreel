1ed2e7e743c190a04c1ceadaa5486dcaf57e34b2
Sebastian Lundqvist <sebastian.lundqvist@combitech.se>
2019-08-21 15:10:38 +0200

    Add themoviedb API functionality

---
+++ lib/services/movies.dart
@@ -0,0 +1,16 @@
+import 'networking.dart';
+
+const apiKey = 'bc0a0c6901ec87ac2bd09dc2264bb1af';
+const url = 'https://api.themoviedb.org/3';
+
+class Movies {
+  Future<dynamic> getMovies(int minReleaseDate, int maxReleaseDate,
+      int minVoteCount, double minVoteAverage) async {
+    String completeURL =
+        '$url/discover/movie?api_key=$apiKey&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&primary_release_date.gte=$minReleaseDate&primary_release_date.lte=$maxReleaseDate';
+    NetworkHelper networkHelper = NetworkHelper(completeURL);
+    print(completeURL);
+    var movieData = await networkHelper.getData();
+    return movieData;
+  }
+}
