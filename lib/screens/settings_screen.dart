82e51acc0b9e2db1e4435bee45778952d17c3cf2
Sebastian Lundqvist <sebastian.lundqvist@combitech.se>
2019-08-27 16:18:34 +0200

    Add minimum rating slider

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -6,10 +6,34 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double _voteAverageValue = 5.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings'),
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(child: Text('Min. Rating')),
+              Expanded(
+                flex: 3,
+                child: Slider(
+                  min: 0.0,
+                  max: 10.0,
+                  onChanged: (newRating) {
+                    setState(() => _voteAverageValue = newRating);
+                  },
+                  value: _voteAverageValue,
+                  divisions: 10,
+                  label: _voteAverageValue.toString(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
