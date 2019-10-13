496bf255a7ff72f1205aae547fdba84f26ab3811
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-13 21:43:24 +0200

    Add minimum number of votes and year sliders

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,32 +7,81 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   double _voteAverageValue = 5.0;
+  int _minimumVotes = 0;
+  RangeValues _rangeValues = RangeValues(1940, DateTime.now().year.toDouble());
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(child: Text('Min. Rating')),
-              Expanded(
-                flex: 3,
-                child: Slider(
-                  min: 0.0,
-                  max: 10.0,
-                  onChanged: (newRating) {
-                    setState(() => _voteAverageValue = newRating);
-                  },
-                  value: _voteAverageValue,
-                  divisions: 10,
-                  label: _voteAverageValue.toString(),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(child: Text('Min. Rating')),
+                Expanded(
+                  flex: 3,
+                  child: Slider(
+                    activeColor: Colors.amber,
+                    min: 0.0,
+                    max: 10.0,
+                    onChanged: (newRating) {
+                      setState(() => _voteAverageValue = newRating);
+                    },
+                    value: _voteAverageValue,
+                    divisions: 10,
+                    label: _voteAverageValue.toString(),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(child: Text('Min. Votes')),
+                Expanded(
+                  flex: 3,
+                  child: Slider(
+                    activeColor: Colors.amber,
+                    min: 0,
+                    max: 10000,
+                    onChanged: (newVotes) {
+                      setState(() => _minimumVotes = newVotes.toInt());
+                    },
+                    value: _minimumVotes.toDouble(),
+                    divisions: 10,
+                    label: _minimumVotes.toString(),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(child: Text('Year')),
+                Expanded(
+                  flex: 3,
+                  child: RangeSlider(
+                    activeColor: Colors.amber,
+                    min: DateTime.now().year.toDouble() - 100,
+                    max: DateTime.now().year.toDouble(),
+                    onChanged: (RangeValues values) {
+                      setState(() => _rangeValues = values);
+                    },
+                    values: _rangeValues,
+                    divisions: 100,
+                    labels: RangeLabels(
+                      _rangeValues.start.toInt().toString(),
+                      _rangeValues.end.toInt().toString(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
