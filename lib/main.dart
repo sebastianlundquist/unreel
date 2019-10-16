bbbc5136beaecd0e74936b1804bef315e6b35081
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-16 18:48:26 +0200

    Lock screen rotation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movie_app/screens/navigation_screen.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.dark,
