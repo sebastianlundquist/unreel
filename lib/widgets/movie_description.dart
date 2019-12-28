f812491cb1004ab1b08bccf87fca75cf6acc4969
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-12-28 14:49:56 +0100

    Fade bottom of movie description when it goes out of frame

--- lib/widgets/movie_description.dart
+++ lib/widgets/movie_description.dart
@@ -1,3 +1,4 @@
+import 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -29,69 +30,73 @@ class MovieDescription extends StatelessWidget {
       }
     }
     return Expanded(
-      child: ListView(
-        padding: EdgeInsets.all(0.0),
-        children: <Widget>[
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Text(
-              movieObject != null ? movieObject.tagline : '',
-              style: Theme.of(context)
-                  .textTheme
-                  .body1
-                  .copyWith(fontStyle: FontStyle.italic),
-              textAlign: TextAlign.center,
+      child: FadingEdgeScrollView.fromScrollView(
+        gradientFractionOnEnd: 0.15,
+        child: ListView(
+          controller: ScrollController(),
+          padding: EdgeInsets.all(0.0),
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Text(
+                movieObject != null ? movieObject.tagline : '',
+                style: Theme.of(context)
+                    .textTheme
+                    .body1
+                    .copyWith(fontStyle: FontStyle.italic),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Text(
-              movieObject != null ? movieObject.overview : '',
-              style: Theme.of(context).textTheme.body1,
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Text(
+                movieObject != null ? movieObject.overview : '',
+                style: Theme.of(context).textTheme.body1,
+              ),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Column(
-              children: <Widget>[
-                if (movieObject != null &&
-                    movieObject.title != movieObject.originalTitle)
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Column(
+                children: <Widget>[
+                  if (movieObject != null &&
+                      movieObject.title != movieObject.originalTitle)
+                    DescriptionRow(
+                      title: "Original Title:",
+                      description: movieObject.originalTitle,
+                    ),
                   DescriptionRow(
-                    title: "Original Title:",
-                    description: movieObject.originalTitle,
+                    title: "Genres:",
+                    description: genres.join(', '),
                   ),
-                DescriptionRow(
-                  title: "Genres:",
-                  description: genres.join(', '),
-                ),
-                DescriptionRow(
-                  title: "Premiere:",
-                  description: movieObject != null
-                      ? DateFormat.yMMMMd()
-                          .format(DateTime.parse(movieObject.releaseDate))
-                      : '',
-                ),
-                DescriptionRow(
-                  title: "Original Language:",
-                  description: movieObject != null
-                      ? findLanguage(movieObject.originalLanguage)
-                      : '',
-                ),
-                DescriptionRow(
-                  title: "Production:",
-                  description: productionCompanies.join(', '),
-                ),
-                DescriptionRow(
-                  title: "Locations:",
-                  description: productionCountries.join(', '),
-                ),
-              ],
+                  DescriptionRow(
+                    title: "Premiere:",
+                    description: movieObject != null
+                        ? DateFormat.yMMMMd()
+                            .format(DateTime.parse(movieObject.releaseDate))
+                        : '',
+                  ),
+                  DescriptionRow(
+                    title: "Original Language:",
+                    description: movieObject != null
+                        ? findLanguage(movieObject.originalLanguage)
+                        : '',
+                  ),
+                  DescriptionRow(
+                    title: "Production:",
+                    description: productionCompanies.join(', '),
+                  ),
+                  DescriptionRow(
+                    title: "Locations:",
+                    description: productionCountries.join(', '),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
